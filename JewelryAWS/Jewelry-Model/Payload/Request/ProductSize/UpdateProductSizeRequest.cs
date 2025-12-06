eb9aad94b781e730482cd1d2c5a853358d9afee9
HieuNguyen267 <hieundse185047@fpt.edu.vn>
2025-12-06 12:32:53 +0700

    s3 config

---
+++ JewelryAWS/Jewelry-Model/Payload/Request/ProductSize/UpdateProductSizeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Jewelry_Model.Payload.Request.ProductSize;
+
+public class UpdateProductSizeRequest
+{
+    public double? Price { get; set; }
+    
+    public int? Quantity { get; set; }
+}
---
+++ JewelryAWS/Jewelry-Service/Interfaces/IProductSizeService.cs
@@ -0,0 +1,12 @@
+﻿using Jewelry_Model.Payload;
+using Jewelry_Model.Payload.Request.ProductSize;
+using Jewelry_Model.Payload.Response.ProductSize;
+
+namespace Jewelry_Service.Interfaces;
+
+public interface IProductSizeService
+{
+    Task<BaseResponse<bool>> DeleteProductSize(Guid id);
+    
+    Task<BaseResponse<GetProductSizeResponse>> UpdateProductSize(Guid id, UpdateProductSizeRequest request); 
+}
